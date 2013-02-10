525360a5d4b9c90f098af8a97bd95ca029d8b9f7
Rodrigo Pavano <rodrigopavano@gmail.com>
2013-02-10 19:31:15 -0300

    added x and y properties to block

--- src/Block.js
+++ src/Block.js
@@ -3,8 +3,8 @@
         this.displayObject = new createjs.Bitmap();
         this.displayObject.regX = TILE_WIDTH / 2 | 0;
         this.displayObject.regY = TILE_HEIGHT / 2 | 0;
-        this.displayObject.x = x;
-        this.displayObject.y = y;
+        this.x = this.displayObject.x = x;
+        this.y = this.displayObject.y = y;
 
         this.init(resources['block']);
     }
