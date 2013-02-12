3a4da1e063dfe6194979bb4f4e59f100242a617e
Eduardo Ojeda <eduojeda@gmail.com>
2013-02-12 14:00:34 -0300

    no idea what changed here. For great good\!

--- src/Player.js
+++ src/Player.js
@@ -2,31 +2,29 @@ const MOVEMENT_FORCE = 50;
 
 (function (window) {
     function Player(x, y, spriteImg, keyMap) {
+        this.keyMap = keyMap;
+        this.pos = new Vector(x, y);
+        this.vel = new Vector(0, 0);
+        this.force = new Vector(0, 0);
+        this.movement = new Vector(0, 0);
+
         this.displayObject = new createjs.BitmapAnimation();
         this.displayObject.regX = TILE_WIDTH / 2 | 0;
         this.displayObject.regY = TILE_HEIGHT / 2 | 0;
-        this.displayObject.x = x;
-        this.displayObject.y = y;
-
-        this.keyMap = keyMap;
-        //Vectors
-        this.pos = new Vector(x, y);
-        this.movement = new Vector(0, 0);
-        this.vel = new Vector(0, 0);
-
-        this.force = new Vector(0, 0);
-
-        this.activeStateHandler = this.stateHandlers.idle;
-        this.init(spriteImg);
 
         // The onTick hander has to be set here so that the closure has access to the Player instance
         var that = this;
         this.displayObject.onTick = function() {
             that.tick();
         };
+
+        this.activeStateHandler = this.stateHandlers.idle;
+        this.init(spriteImg);
     }
 
     Player.prototype.tick = function (e) {
+
+
         //@todo doThePhysics()
         this.displayObject.x = this.pos.x;
         this.displayObject.y = this.pos.y;
