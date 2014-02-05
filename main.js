d7d25c7de1a6083335b19296c4ff5a5d1dd4b23f
eduojeda <eduojeda@gmail.com>
2014-02-05 22:38:19 +0100

    Now loading resources with manifest files

--- main.js
+++ main.js
@@ -9,42 +9,27 @@ const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
 
 var canvas;
-var gScreenWidth;
-var gScreenHeight;
 var resources = {};
 
-window.onload = init;
-
-function init() {
+window.onload = function() {
     canvas = document.getElementById("dasCanvas");
-    gScreenWidth = canvas.width;
-    gScreenHeight = canvas.height;
-
     loadResources();
-    loadSources();
-}
+};
 
 function loadResources() {
-    var manifest = [
-        {src:"res/img/solid_block.png", id:"block"},
-        {src:"res/img/player.png", id:"player1"}
-    ];
+    var preloader = new createjs.LoadQueue(false);
 
-    var queue = new createjs.LoadQueue(false);
+    preloader.on("progress", function(event) {
+        var ctx = canvas.getContext("2d"),
+            angle = Math.PI * 2 * event.loaded;
 
-    queue.on("progress", function(event) {
-        var ctx = canvas.getContext("2d");
-
-        canvas.width = canvas.width; // clear canvas
-
-        var angle = Math.PI * 2 * event.loaded;
         ctx.strokeStyle = "white";
         ctx.beginPath();
-        ctx.arc(gScreenWidth/2, gScreenHeight/2, 100, 0, angle, true);
+        ctx.arc(canvas.width/2, canvas.height/2, 100, 0, angle, true);
         ctx.stroke();
     });
 
-    queue.on("fileload", function(event) {
+    preloader.on("fileload", function(event) {
         var item = event.item;
         switch(item.type) {
             case createjs.LoadQueue.IMAGE:
@@ -55,42 +40,26 @@ function loadResources() {
 
             case createjs.LoadQueue.SOUND:
                 break;
-        }
-    });
 
-    queue.loadManifest(manifest);
-}
-
-function loadSources() {
-    var manifest = [
-        {src:"src/Vector.js", id:"vector"},
-        {src:"src/World.js", id:"world"},
-        {src:"src/Block.js", id:"block"},
-        {src:"src/Player.js", id:"player"}
-    ];
-
-    var preloader = new createjs.LoadQueue(false);
-
-    preloader.onFileLoad = function(event) {
-        switch(event.type) {
-            case createjs.PreloadJS.JAVASCRIPT:
-                document.body.appendChild(event.result);
+            case createjs.LoadQueue.JAVASCRIPT:
+                // LoadQueue(false) auto-adds scripts to the DOM
                 break;
         }
-    };
+    });
 
     preloader.on("complete", function() {
         //@todo THIS IS BULLSHIT, we should have an 'allLoaded' global flag we check so that we dont depend on the order of the load calls
         setTimeout(function() {
             var world = new World(canvas);
-            //TODO: refactor, we should just call world.init({options}) 
+            //TODO: refactor, we should just call world.init({options})
             world.initLevel();
-            world.initPlayers();
+            world.initPlayer();
             world.initMobs(10);
             world.registerKeyEvents();
             world.start();
         }, 250);
     });
 
-    preloader.loadManifest(manifest);
+    preloader.loadManifest("sources_manifest.json");
+    preloader.loadManifest("resources_manifest.json");
 }
