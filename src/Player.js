02eb4a1de782d6c8a8dd608267bc608836bedc06
eduojeda <eduojeda@gmail.com>
2014-02-24 20:20:43 +0100

    Who knows...

--- src/Player.js
+++ src/Player.js
@@ -16,16 +16,6 @@ entities.Player = function(x, y, keyMap, keyboardState) {
     this._keyboardState = keyboardState;
 };
 
-/*
-@todo Refactor all the state / animation logic. It's awful stuff.
-    We only execute the stateHandlers when the state changed from the previous frame. This is so that we don't call gotoAndPlay
-    every tick (otherwise animation doesnt work, it rewinds on every tick),
-    BUT
-    it means that if we go from moving on one direction to another without going through state idle, the animation doesn't switch
-    to the correct direction (since state didnt change)
-    also, it means we cant do stuff on the stateHandlers that should be done on every frame
- */
-
 entities.Player.prototype = {
     stateHandlers: {
         movingUp: function () {
