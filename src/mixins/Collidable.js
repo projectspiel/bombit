b978557523ad251a6ec3f0c9fde4471d10098677
eduojeda <eduojeda@gmail.com>
2014-07-02 00:32:20 +0200

    Improved isCollidable check to also skip non-entity objects

--- src/mixins/Collidable.js
+++ src/mixins/Collidable.js
@@ -8,7 +8,7 @@ mixins.Collidable = function(callback) {
     this.checkCollisions = function(stage) {
         for(var i = 0 ; i < stage.getNumChildren() ; i++) {
             var entity = stage.getChildAt(i);
-            if (entity === this.getDisplayObject() || !entity.isCollidable) {
+            if (entity === this.getDisplayObject() || !entity.parentEntity || (entity.parentEntity && !entity.parentEntity.isCollidable)) {
                 continue; // Don't collide with self or non-collidables
             }
 
--- src/mixins/Sprite.js
+++ src/mixins/Sprite.js
@@ -13,6 +13,9 @@ mixins.Sprite = function(spriteSheetData) {
         this._displayObject.y = this.pos.y;
     };
 
+    /**
+     * @fixme Kill this method and replace with a list of proxy methods
+     */
     this.getDisplayObject = function() {
         return this._displayObject;
     };
@@ -23,5 +26,12 @@ mixins.Sprite = function(spriteSheetData) {
 mixins.Sprite.init = function() {
     this._displayObject = new createjs.Sprite(this._spriteSheet);
     this._displayObject.scaleX = this._displayObject.scaleY = 2;
+
+    /**
+     * @fixme THIS IS NOT OKAY.
+     * Kill once we replace the collision system
+     */
+    this._displayObject.parentEntity = this;
+
     this.render();
 };
