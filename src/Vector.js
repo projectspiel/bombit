be53d8462b16c91758d08e767a54a559b9a5463a
eduojeda <eduojeda@gmail.com>
2014-08-05 23:15:27 +0200

    Added Z dimension to Vector

--- src/Vector.js
+++ src/Vector.js
@@ -1,40 +1,46 @@
-function Vector(x, y) {
+function Vector(x, y, z) {
     this.x = x || 0;
     this.y = y || 0;
+    this.z = z || 0;
 }
 
 Vector.prototype = {
-    set: function (x1, y1) {
+    set: function (x1, y1, z1) {
         this.x = x1;
         this.y = y1;
+        if (typeof z1 !== "undefined") {this.z = z1;}
     },
     reset: function () {
-        this.set(0, 0);
+        this.set(0, 0, 0);
     },
     clone: function () {
-        return Object.build(Vector, this.x, this.y);
+        return Object.build(Vector, this.x, this.y, this.z);
     },
     add: function (v) {
         this.x += v.x;
         this.y += v.y;
+        this.z += v.z;
         return this;
     },
     substract: function (v) {
         this.x -= v.x;
         this.y -= v.y;
+        this.z -= v.z;
         return this;
     },
     scalar: function (scalar) {
         this.x *= scalar;
         this.y *= scalar;
+        this.z *= scalar;
         return this;
     },
     normalize: function () {
-        var aux = this.x * this.x + this.y * this.y;
+        var aux = this.x * this.x + this.y * this.y + this.z * this.z;
         if (Math.abs(aux - 1.0) > 0.0001 && aux > 0) {	// Don't normalize if it's not needed
             var magnitude = Math.sqrt(aux);
             this.x /= magnitude;
             this.y /= magnitude;
+            this.z /= magnitude;
         }
         return this;
     },
@@ -42,12 +48,12 @@ Vector.prototype = {
         return v.clone().substract(this).normalize();
     },
     modulo: function () {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     },
     distance: function (v) {
-        return Math.sqrt(Math.pow(this.x - v.x, 2) + Math.pow(this.y - v.y, 2));
+        return Math.sqrt(Math.pow(this.x - v.x, 2) + Math.pow(this.y - v.y, 2) + Math.pow(this.z - v.z, 2));
     },
     toString: function () {
-        return "(" + this.x + ", " + this.y + ")";
+        return "(" + this.x + ", " + this.y + ", " + this.z + ")";
     }
 };
