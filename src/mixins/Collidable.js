73cdd94bc82cf6bd67cc4a550e7a9de8723ed51e
Eduardo Ojeda <eduardo.ojeda@researchgate.net>
2015-02-17 22:40:17 +0100

    Collidable now passes the colliding entitiy to the callbacks

--- src/mixins/Collidable.js
+++ src/mixins/Collidable.js
@@ -1,12 +1,15 @@
 var mixins = mixins || {};
 
 mixins.Collidable = function (options) {
-    if (!this.isInitializable || !this.isSimulable || !this.isPositionable || !this.isSprite) { throw "Dependencies not met"; }
+    if (!this.isInitializable || !this.isSimulable || !this.isPositionable || !this.isSprite) {
+        throw "Dependencies not met";
+    }
     this.isCollidable = true;
 
     var callback = options.callback,
         hitAreaRadius = options.hitAreaRadius,
-        collidables = mixins.Collidable.entities;
+        collidables = mixins.Collidable.entities,
+        response = new SAT.Response();
 
     this.onInit(function () {
         this.hitArea = new SAT.Circle(new SAT.Vector(this.pos.x, this.pos.y), hitAreaRadius);
@@ -20,16 +23,15 @@ mixins.Collidable = function (options) {
     this.checkCollisions = function () {
         this.hitArea.pos.x = this.pos.x;
         this.hitArea.pos.y = this.pos.y;
-        var response,
-            collided;
 
-        for (var i = 0 ; i < collidables.length ; i++) {
-            if (this === collidables[i]) { continue; }
-            response = new SAT.Response();
-            collided = SAT.testCircleCircle(this.hitArea, collidables[i].getHitArea(), response);
-            if (collided) {
-                callback.call(this, response);
+        for (var i = 0; i < collidables.length; i++) {
+            if (this === collidables[i]) {
+                continue;
             }
+            if (SAT.testCircleCircle(this.hitArea, collidables[i].getHitArea(), response)) {
+                callback.call(this, response, collidables[i]);
+            }
+            response.clear();
         }
     };
 
