f53631b549fa86634066396d82fb09c07dad4430
Eduardo Ojeda <eduardo.ojeda@researchgate.net>
2015-07-12 22:36:04 +0200

    Made ball less bouncy

--- src/Ball.js
+++ src/Ball.js
@@ -11,10 +11,10 @@ entities.Ball = new entities.Entity({
         }
     },
     physical: {
-        friction: 2,
+        friction: 10,
         mass: 1,
         dampFactor: 0.6,
-        inputForce: 100 //@todo this hacks around the fact that there is no good way of adding a force on construction to a Physical (like when the player makes a Ball to throw), so we use Physical.addInputForce for now
+        inputForce: 70 //@todo this hacks around the fact that there is no good way of adding a force on construction to a Physical (like when the player makes a Ball to throw), so we use Physical.addInputForce for now
     },
     collidable: {
         collisionCallback: function (collision) {
