3e78c91b15a8b4b0ae42f31a9a13749db887cb8d
Rodrigo Pavano <rodrigopavano@gmail.com>
2015-10-12 10:47:08 -0300

    Style fix

--- src/mixins/HasFilters.js
+++ src/mixins/HasFilters.js
@@ -5,18 +5,18 @@ mixins.HasFilters = function () {
 
     // Circular dependenty with Sprite, how do we solve that?
 
-    this.onInit(function() {
+    this.onInit(function () {
         this.sprite.filters = [];
         this._filters = {};
     });
 
-    this.setFilter = function(name, filter) {
+    this.setFilter = function (name, filter) {
       this.removeFilter(name);
       this._filters[name] = filter;
       this.sprite.filters.push(filter);
     };
 
-    this.removeFilter = function(name) {
+    this.removeFilter = function (name) {
       this.sprite.filters.forEach(
         (filter, index) => {
           if (filter === this._filters[name]){
