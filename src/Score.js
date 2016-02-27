d03f8b8f07dd4d624e5abc7d21d008d403327865
Eduardo Ojeda <eduardo.ojeda@researchgate.net>
2016-02-27 11:53:48 +0100

    Added score animation for level transition

--- src/Score.js
+++ src/Score.js
@@ -8,21 +8,44 @@ var Score = (function () {
     text.zindex = 1000;
 
     function increment() {
-      level++;
-      update();
+        level++;
+        update();
     }
 
     function getDisplayObject() {
-      return text;
+        return text;
     }
 
     function update() {
-      text.text = "Level " + level;
+        text.text = "Level " + level;
+        animateLevelChange();
+    }
+
+    function animateLevelChange() {
+        var color = 255,
+            setColor = (color) => {
+                text.color = "rgb(" + color + ", " + color + ", " + color + ")";
+            };
+
+        text.scaleX = 5;
+        text.scaleY = 5;
+        setColor(color);
+        text.on("tick", (e) => {
+            text.scaleX -= 0.1;
+            text.scaleY -= 0.1;
+
+            setColor(color -= 5);
+            if (text.scaleX <= 1) {
+                text.scaleX = 1;
+                text.scaleY = 1;
+                setColor(0);
+                e.remove();
+            }
+        });
     }
 
     function reset() {
         level = 0;
-        update();
     }
 
     return {
